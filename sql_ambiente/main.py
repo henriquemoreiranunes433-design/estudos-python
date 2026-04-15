3eeda94d3777c6f531252dd4fdaa6197c2191337
Henrique Moreira <henriquemoreiranunes433@gmail.com>
2026-04-15 09:19:21 -0300

    criando tabela sql e inserindo dados

--- sql_ambiente/main.py
+++ sql_ambiente/main.py
@@ -4,17 +4,35 @@ import os
 
 dotenv.load_dotenv()
 
+TABLE_NAME = 'customers'
 # CONECTANDO A BASE DE DADOS AO PYMYSQL
-cconnection = pymysql.connect(
+connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],       
     password=os.environ['MYSQL_PASSWORD'],  
     database=os.environ['MYSQL_DATABASE']
 )
 # CONECTANDO O CURSOR
-cursor = cconnection.cursor()
+cursor = connection.cursor()
 
+cursor.execute(f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+        id INT NOT NULL AUTO_INCREMENT,
+        nome VARCHAR(50) NOT NULL,
+        idade INT NOT NULL,
+        PRIMARY KEY (id)
+    )
+""")
 
+cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+
+cursor. execute(f"""
+    INSERT INTO {TABLE_NAME} 
+    (nome,idade) VALUES ("Henrique",22)
+
+""")
+
+connection.commit()
 # FECHANDO CONEXÕES
 cursor.close()
-cconnection.close()
+connection.close()
